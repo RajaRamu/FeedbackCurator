6e87c186c86b81c8e9147893b0e49102f618a6b6
RajaRamu <raja.skk@gmail.com>
2014-11-19 01:15:43 -0800

    Feedback curator
    A hackathon project

---
+++ README.md
@@ -0,0 +1,3 @@
+# FeedbackCurator
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/FeedbackCuratorController.js
@@ -0,0 +1,11 @@
+/**
+ * FeedbackCuratorController
+ *
+ * @description :: Server-side logic for managing Feedbackcurators
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
