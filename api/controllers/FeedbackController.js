43ca65155049df8d28fbda5649bb02804f6f7ff1
niazanazniaz <niazanazniaz@gmail.com>
2014-11-24 14:54:57 -0800

    changed update

--- api/controllers/FeedbackController.js
+++ api/controllers/FeedbackController.js
@@ -11,24 +11,25 @@ module.exports = {
 		Feedback.find({'feedback_id': req.param("fid")}).exec(function foundFeedbacks(err, feeds){
 			if(err) return next(err);
 			res.json(feeds);
-			//res.view({feedbacks : feedbacks})
 		});
-		//res.view();
 	},
 
 	updatefeed : function (req, res){
 		Feedback.update(
 			{'feedback_id': req.param("fid")},
-			{'feedback_Question' : req.param("fq"),
+			{'feedback_Question.How accurate was the item description' : req.param("q1"),
+			'feedback_Question.How quickly did the seller ship the item' : req.param("q2"),
+			'feedback_Question.How reasonable were the shipping and handling charges' : req.param("q3"),
+			"feedback_Question.How satisfied were you with the seller's communication" : req.param("q4"),
 			 'feedback_comments' : req.param("fc"),
 			 'feedback_rating' : req.param("fr")
 		    }
 			).exec(function(err, feeds){
 			if(err) {return next(err);}
 			return res.send('success');
-			//res.view({feedbacks : feedbacks})
 		});
-		//res.view();
 	}
 	
 };
+
+
--- api/controllers/FeedbackCuratorController.js
+++ api/controllers/FeedbackCuratorController.js
@@ -17,21 +17,19 @@ sellerview : function (req, res){
 },
 
 sendoffer : function (req, res){
-		
  if (req.param("fid") != null && req.isSocket){
-
- 	var feedback_id =	{'feedback_id': req.param("fid")};
-			var offer_details = {'offer_details' : req.param("od") };
-
- 	FeedbackCurator.update(
-			feedback_id,
-			offer_details
+	FeedbackCurator.update(
+			{'feedback_id': req.param("fid")},
+			{'offer_details.discount' : req.param("discount"),
+			 'offer_details.offer_info' : req.param("offer_info"),
+			 'offer_details.offer_sent' : req.param("offer_sent"),
+			 'offer_details.offer_status' : req.param("offer_status"),
+			 'offer_details.refund_amount' : req.param("refund_amount"),
+		    }
 			).exec(function(err, feeds){
 			if(err) {return next(err);}
 		FeedbackCurator.publishCreate({id:sails.sockets.id(req),feedback_id:feedback_id.feedback_id,offer_details:offer_details.offer_details});
             console.log('SEnd offer agent has been created');
-			//return res.send('success');
-			//res.view({feedbacks : feedbacks})
 		});
  } else if (req.isSocket){
 
@@ -40,14 +38,18 @@ sendoffer : function (req, res){
 
   }  else {
 
- 	FeedbackCurator.update(
-			feedback_id,
-			offer_details
+	FeedbackCurator.update(
+			{'feedback_id': req.param("fid")},
+			{'offer_details.discount' : req.param("discount"),
+			 'offer_details.offer_info' : req.param("offer_info"),
+			 'offer_details.offer_sent' : req.param("offer_sent"),
+			 'offer_details.offer_status' : req.param("offer_status"),
+			 'offer_details.refund_amount' : req.param("refund_amount"),
+		    }
 			).exec(function(err, feeds){
 			if(err) {return next(err);}
 	
 			return res.send('success');
-			//res.view({feedbacks : feedbacks})
 		});
 
  }
@@ -55,27 +57,26 @@ sendoffer : function (req, res){
 
 },
 
-
 buyerview : function (req, res){
 	FeedbackCurator.find({'buyer_id': req.param("bid")}).exec(function foundUsers(err, feedbacks){
 		if(err) return next(err);
 		res.json(feedbacks);
-		//res.view({feedbacks : feedbacks})
 	});
-	//res.view();
 },
 
 acceptoffer : function (req, res){
 	FeedbackCurator.update(
 			{'feedback_id': req.param("fid")},
-			{'offer_details' : req.param("od")
+			{'offer_details.discount' : req.param("discount"),
+			 'offer_details.offer_info' : req.param("offer_info"),
+			 'offer_details.offer_sent' : req.param("offer_sent"),
+			 'offer_details.offer_status' : req.param("offer_status"),
+			 'offer_details.refund_amount' : req.param("refund_amount"),
 		    }
 			).exec(function(err, feeds){
 			if(err) {return next(err);}
 			return res.send('success');
-			//res.view({feedbacks : feedbacks})
 		});
-	//res.view();
 }
 
 
